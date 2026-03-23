cad5fb7bf96ea2059bfc3ebe5830c3cc0b62ae96
APXs-01 <pradeepsripal.com@gmail.com>
2026-03-23 16:56:57 +0530

    Admin Controller - Financial Dashboard & Platform Management

---
+++ backend/controllers/admin.controller.js
@@ -0,0 +1,14 @@
+// Admin Controller - Financial Dashboard & Platform Management
+// Member 1 - Admin financial dashboard (revenue, trends, payout summaries)
+// Member 2 - Admin order & review management
+// Member 3 - Admin community moderation
+// Member 4 - Admin user management
+const User = require("../models/User");
+const Order = require("../models/Order");
+const Transaction = require("../models/Transaction");
+const Review = require("../models/Review");
+const Gig = require("../models/Gig");
+const Community = require("../models/Community");
+const PayoutRequest = require("../models/PayoutRequest");
+const Badge = require("../models/Badge");
+const Discount = require("../models/Discount");
