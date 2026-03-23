5835bdb92f16c5d66654f34792d4a3a2525658e9
amasha1797 <it23647282@my.sliit.lk>
2026-03-23 16:33:53 +0530

    controller/order.controller.js/Review Controller (Rating & Review System)

---
+++ backend/controllers/review.controller.js
@@ -0,0 +1,5 @@
+// Member 2 - Review Controller (Rating & Review System)
+const Review = require("../models/Review");
+const Order = require("../models/Order");
+const Gig = require("../models/Gig");
+const { createNotification } = require("../utils/notificationHelper");
