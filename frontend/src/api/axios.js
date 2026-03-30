9e8680bfaf6a072a61d4e99cbdca0df917062112
APXs-01 <pradeepsripal.com@gmail.com>
2026-03-30 11:11:27 +0530

    frontend/src/api/axios.js Attach JWT token to every request automatically

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,6 +6,11 @@ const API = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-
+// Attach JWT token to every request automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("unilance_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default API;
